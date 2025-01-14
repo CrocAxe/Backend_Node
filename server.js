e616583d3e4a17bf6c917f15a82df890f5917ec5
Comfort Ngwenya <nqobilecomfyngwenya@gmail.com>
2025-01-14 23:20:39 +0200

    Adding the profile route with the verify auth middleware

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require("express");
 const cors = require("cors");
 const { db, auth } = require("./config/firebase");
 const userRoutes = require("./routes/authRoutes");
-
+const profileRoutes = require("./routes/profileRoutes");
+const verifyAuth = require("./middleware/authMiddleware")
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // Routes for Users
 app.use("/users", userRoutes);
+app.use("/profile", verifyAuth, profileRoutes);
 
 // Default Route
 app.get("/", (req, res) => {
