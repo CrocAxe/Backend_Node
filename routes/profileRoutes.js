6bd9dc69ba692d0362221b472c3e454b247ea764
Comfort Ngwenya <nqobilecomfyngwenya@gmail.com>
2025-01-17 09:14:45 +0200

    Adding the favorites routing for the profile

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { getProfile, updateProfile } = require("../controllers/profileController");
+const { getProfile, updateProfile, addFavorite, getFavorites, removeFavorite } = require("../controllers/profileController");
 
 const router = express.Router();
 
 router.get("/get-profile", getProfile);
 router.post("/update-profile", updateProfile);
+router.post('/add-favorites', addFavorite);
+router.delete('/favorites/:placeId', removeFavorite);
+router.get('/favorites', getFavorites);
 
 module.exports = router;
